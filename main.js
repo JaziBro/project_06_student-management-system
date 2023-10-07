6ddb7dca4ab8929a276e3af549c67292f02dbebe
jazilhashmi <138767638+JaziBro@users.noreply.github.com>
2023-10-07 10:39:33 -0700

    .

--- main.js
+++ main.js
@@ -17,6 +17,10 @@ class Student extends Person {
     enrollForCourses(course) {
         this.courses.push(course);
     }
+    createPin() {
+        let pin = Math.round(Math.random() * 1000);
+        console.log(pin);
+    }
 }
 class Teacher extends Person {
     constructor(name, age, salary) {
@@ -45,14 +49,14 @@ class Course {
 }
 const student1 = new Student("Jazil", 16, 2345);
 const student2 = new Student(`Sulaiman`, 16, 6662);
-console.log(student1);
-console.log(student2);
-const teacher1 = new Teacher(`Zia`, 65, 50000);
-const teacher2 = new Teacher(`Hamza`, 22, 30000);
-teacher1.assignCourse(`Metaverse`);
-teacher2.assignCourse(`AI`);
-console.log(teacher1);
-const course1 = new Course(`course1`, `AI`);
-course1.addStudent(student1);
-course1.addStudent(student2);
-console.log(course1);
+console.log(`your pincode is`, student1.createPin());
+// console.log(student2)
+// const teacher1 = new Teacher(`Zia`, 65, 50000)
+// const teacher2 = new Teacher(`Hamza`, 22,30000 )
+// teacher1.assignCourse(`Metaverse`)
+// teacher2.assignCourse(`AI`)
+// console.log(teacher1)
+// const course1 = new Course(`course1`, `AI`)
+// course1.addStudent(student1)
+// course1.addStudent(student2)
+// console.log(course1)
